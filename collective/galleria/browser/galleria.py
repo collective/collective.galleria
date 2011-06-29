32d233490a55f249d6479b31abbe3d7b608ca8c3
JeanMichel FRANCOIS aka toutpt <toutpt@gmail.com>
2011-06-29 16:31:13 +0200

    add options

--- collective/galleria/browser/galleria.py
+++ collective/galleria/browser/galleria.py
@@ -3,9 +3,17 @@ import json
 from zope import component
 from zope import interface
 from zope import schema
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from collective.configviews import ConfigurableBaseView
 
 from collective.galleria import i18n
+
+transitions = SimpleVocabulary(
+    [SimpleTerm(value='fade', title=i18n.fade),
+     SimpleTerm(value='flash', title=i18n.flash),
+     SimpleTerm(value='pulse', title=i18n.pulse),
+     SimpleTerm(value='slide', title=i18n.slide),
+     SimpleTerm(value='fadeslide', title=i18n.fadeslide)])
 
 class IGalleriaSettings(interface.Interface):
     """http://galleria.aino.se/docs/1.2/options/
@@ -21,26 +29,59 @@ class IGalleriaSettings(interface.Interface):
                                  description=i18n.carouselFollow_desc,
                                  default=True)
 
+
+    showInfo = schema.Bool(title=i18n.showInfo,
+                          description=i18n.showInfo_desc,
+                          default=True)
+
+    showCounter = schema.Bool(title=i18n.showCounter,
+                          description=i18n.showCounter_desc,
+                          default=True)
+
+    showImagenav = schema.Bool(title=i18n.showImagenav,
+                          description=i18n.showImagenav_desc,
+                          default=True)
+
+
+    transition = schema.Choice(title=i18n.transition,
+                          description=i18n.transition_desc,
+                          default='fade',
+                          vocabulary=transitions)
+
+    transitionSpeed = schema.Int(title=i18n.transitionSpeed,
+                        default=400)
+
+    pauseOnInteraction = schema.Bool(title=i18n.pauseOnInteraction,
+                          description=i18n.pauseOnInteraction_desc,
+                          default=True)
+
     width = schema.ASCIILine(title=i18n.width,
                            description=i18n.width_desc,
-                           default='auto')
+                           default='550')
+
+    height = schema.Int(title=i18n.height,
+                        default=320)
+
+    debug = schema.Bool(title=i18n.debug,
+                        description=i18n.debug_desc,
+                        default=False)
 
 class Galleria(ConfigurableBaseView):
     settings_schema = IGalleriaSettings
     jsvarname = 'galleriaconfig'
     settings_providers = ('context.zope.annotation',)
     jsdata = 'galleriedata'
-
-    def gallery_data(self):
-        gallery = component.getMultiAdapter((self.context, self.request),
-                                            name="gallery")
-        photos = gallery.photos()
-        data = []
-        for photo in photos:
-            info = {'thumb': photo.thumb_url,
-                    'image': photo.url,
-                    'title': photo.title,
-                    'description': photo.descripiton}
-                    #link: 'http://my.destination.com
-            data.append(info)
-        return self.jsdata + '= ' + json.dumps(data)
+#
+#    def gallery_data(self):
+#        gallery = component.getMultiAdapter((self.context, self.request),
+#                                            name="gallery")
+#        photos = gallery.photos()
+#        data = []
+#        for photo in photos:
+#            info = {'thumb': photo.thumb_url,
+#                    'image': photo.url,
+#                    'title': photo.title,
+#                    'description': photo.descripiton}
+#                    #link: 'http://my.destination.com
+#            data.append(info)
+#        return self.jsdata + '= ' + json.dumps(data)
--- collective/galleria/i18n.py
+++ collective/galleria/i18n.py
@@ -31,6 +31,67 @@ carouselFollow_desc = _(u"carouseilFollow_desc",
                         lags in the main animation you can try to set this
                         option to false.""")
 
+showInfo = _(u"showInfo",default=u"show Info")
+showInfo_default = u"""Set this to false if you do not wish to display the caption."""
+showInfo_desc =_(u"showInfo_desc", default=showInfo_default)
+
+showCounter = _(u"showCounter", default=u"show Counter")
+showCounter_default=u"""Set this to false if you do not wish to display the counter."""
+showCounter_desc = _(u"showCounter_desc", default=showCounter_default)
+
+showImagenav = _(u"showImagenav", default=u"show Imagenav")
+showImagenav_default=u"""Set this to false if you do not wish to display the image navigation (next/prev arrows)."""
+showImagenav_desc = _(u"showImagenav_desc", default=showImagenav_default)
+
+transition = _(u"transition", default=u"transition")
+transition_default=u"""The transition that is used when displaying the images. There are some built-in transitions in Galleria, but you can also create your own using our Transitions API."""
+transition_desc = _(u"transition_desc", default=transition_default)
+
+debug = _(u"debug", default=u"Debug")
+debug_default=u"""This option is for turning debug on/off. By default, Galleria displays errors by printing them out in the gallery container and sometimes throw exceptions. For deployment you can turn debug off to generate a more generic error message if a fatal error is raised."""
+debug_desc = _(u"debug_desc", default=debug_default)
+
+pauseOnInteraction = _(u"pauseOnInteraction", default=u"pause on interaction")
+pauseOnInteraction_default=u"""During playback, Galleria will stop the playback if the user presses thumbnails or any other navigational links. If you dont want this behaviour, set this option to false."""
+pauseOnInteraction_desc = _(u"pauseOnInteraction_desc", default=pauseOnInteraction_default)
+
+transitionSpeed = _(u"transitionSpeed", default=u"Transition speed")
+transitionSpeed_default=u"""The milliseconds used in the animation when applying the transition. The higher number, the slower transition."""
+transitionSpeed_desc = _(u"transitionSpeed_desc", default=transitionSpeed_default)
+
+# = _(u"", default=u"")
+#_default=u""" """
+#_desc = _(u"_desc", default=_default)
+#
+# = _(u"", default=u"")
+#_default=u""" """
+#_desc = _(u"_desc", default=_default)
+
+# = _(u"", default=u"")
+#_default=u""" """
+#_desc = _(u"_desc", default=_default)
+#
+# = _(u"", default=u"")
+#_default=u""" """
+#_desc = _(u"_desc", default=_default)
+
+# = _(u"", default=u"")
+#_default=u""" """
+#_desc = _(u"_desc", default=_default)
+#
+# = _(u"", default=u"")
+#_default=u""" """
+#_desc = _(u"_desc", default=_default)
+
 width = _(u"width",default="Width")
 width_desc = _(u"width_desc",
                default=u"""By default, Galleria fetches the width from the containing element. Bu you can use this option to set a gallery width manually.""")
+
+height = _(u"height",default="Height")
+
+#vocabulary
+fade = _(u'Fade')
+flash= _(u'Flash')
+pulse = _(u'Pulse')
+slide = _(u'Slide')
+fadeslide = _(u'Fade slide')
